9a5e554aa6246437f1e0bfe512ebf0f7d8f33b18
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-15 23:07:14 +0530

    Update Add Label to Panel programmatically.cs

--- WinForm/Add Label to Panel programmatically.cs
+++ WinForm/Add Label to Panel programmatically.cs
@@ -1,4 +1,8 @@
         private void makeCell(Panel p, int w, string txt, Color bg)
         {            
+            Label l = new Label();
+            l.BackColor = bg;
+            l.ForeColor = Color.FromArgb(175, 175, 175);
+            l.AutoSize = false;
 
         }
