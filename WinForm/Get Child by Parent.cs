730bdb02b4f0ac2ec6cc493ae9fae1cd959d8875
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-21 23:10:49 +0530

    Update Get Child by Parent.cs

--- WinForm/Get Child by Parent.cs
+++ WinForm/Get Child by Parent.cs
@@ -1,4 +1,7 @@
         private string getRowId(object sender)
         {
-
+            var b = sender as Button;
+            Panel p =(Panel)b.Parent;
+            Label l = (Label)p.GetChildAtPoint(new Point(5,5));
+            return l.Text;
         }
