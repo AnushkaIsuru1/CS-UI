7dfeedc641827c1424bf1f138b0bad17b57e44d9
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-23 23:12:12 +0530

    Update List Box.cs

--- WinForm/List Box.cs
+++ WinForm/List Box.cs
@@ -1,5 +1,10 @@
 Boolean s = true;
             for(int i = 0; i< listBox1.Items.Count; i++)
             {
-  
+                if (listBox1.Items[i].ToString().ToLower() == textBox1.Text.Trim().ToLower())
+                {
+                    listBox1.SelectedIndex = i;
+                    s = false;
+                    break;
+                }
             }
