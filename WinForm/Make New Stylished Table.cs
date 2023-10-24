e46c9768a396e3a416c66851e5c24ab722d02600
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-24 23:13:16 +0530

    Update Make New Stylished Table.cs

--- WinForm/Make New Stylished Table.cs
+++ WinForm/Make New Stylished Table.cs
@@ -7,7 +7,12 @@
             l.Height = 40;
             l.Width = w;
             l.Dock = DockStyle.Left;
-
+            l.Padding = new Padding(10, 0, 10, 0);
+            l.TextAlign = ContentAlignment.MiddleLeft;
+            if (w == 60) { l.TextAlign = ContentAlignment.MiddleCenter; }
+            l.Text = txt;
+            l.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            p.Controls.Add(l);
         }
 
 
