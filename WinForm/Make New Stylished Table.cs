88b9a73e16141a203a0b1341890407fcd523e363
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-24 23:13:20 +0530

    Update Make New Stylished Table.cs

--- WinForm/Make New Stylished Table.cs
+++ WinForm/Make New Stylished Table.cs
@@ -17,7 +17,14 @@
 
         private void makeBtn(Panel p, Boolean t)
         {
-
+            //Config Edit button
+            Color bg = Color.FromArgb(45,45,45);
+            Color hovwe = Color.FromArgb(70,70,70);
+            Color active = Color.FromArgb(90,90,90);
+            Color boader = Color.FromArgb(100,100,100);
+            EventHandler e = new EventHandler(this.edit);
+            int lef = 700;
+            string txt = "EDIT";
             
 
 
