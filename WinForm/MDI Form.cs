2ee4813996446b317de39dc35efb284b9fa86293
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-26 23:14:45 +0530

    Update MDI Form.cs

--- WinForm/MDI Form.cs
+++ WinForm/MDI Form.cs
@@ -3,6 +3,10 @@
 
         private void FileMenu_Click(object sender, EventArgs e)
         {
-
+            
+            f1.MdiParent = this;
+            f2.Visible = false;
+            f1.Visible = true;
+            f1.Show();
         }
 
