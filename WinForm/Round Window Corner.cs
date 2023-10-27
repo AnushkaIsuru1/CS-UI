e497536879cd05a46bdbcb4e5500ddc86c8f520a
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-27 23:17:13 +0530

    Update Round Window Corner.cs

--- WinForm/Round Window Corner.cs
+++ WinForm/Round Window Corner.cs
@@ -4,6 +4,10 @@ using System.Runtime.InteropServices;
 
 public partial class Form1 : Form
 {
-
+    public Form1()
+    {
+            InitializeComponent();
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+    }
 
 }
