8064243d973e296660b9ddd877623655b4d82aff
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-28 23:17:56 +0530

    Update Searcjing in DataBase.cs

--- WinForm/Searcjing in DataBase.cs
+++ WinForm/Searcjing in DataBase.cs
@@ -1,4 +1,7 @@
         private void TbSearch_TextChanged(object sender, EventArgs e)
         {
+            hideMessage();
+            TextBox t = sender as TextBox;
+            string s = t.Text;
 
         }
