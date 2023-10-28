65303847eb9b58fc935aafbaad69c3f3138874a5
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-28 23:17:58 +0530

    Update Searcjing in DataBase.cs

--- WinForm/Searcjing in DataBase.cs
+++ WinForm/Searcjing in DataBase.cs
@@ -3,5 +3,9 @@
             hideMessage();
             TextBox t = sender as TextBox;
             string s = t.Text;
-
+            if (s != "")
+            {
+                tableWrite($"SELECT `id`, `nm`, `snm`, `un`, `addr` FROM `user` WHERE nm LIKE('{s}%') OR snm LIKE('{s}%') OR un LIKE('{s}%') OR addr LIKE('{s}%');");
+            }
+   
         }
