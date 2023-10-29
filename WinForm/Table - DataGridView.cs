832995cda6752878378d38eed8db59ce6c4e957a
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-30 00:57:22 +0530

    Update Table - DataGridView.cs

--- WinForm/Table - DataGridView.cs
+++ WinForm/Table - DataGridView.cs
@@ -9,6 +9,11 @@ namespace WindowsFormsApp4
 {
     public partial class Form1 : Form
     {
+        public Form1()
+        {
+            InitializeComponent();
+            //this.Controls.Add
+        }
 
 
     }
