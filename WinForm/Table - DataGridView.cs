9c3b4bde9b1941fa15580677fab6136c4fe73033
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-10-30 00:57:20 +0530

    Update Table - DataGridView.cs

---
+++ WinForm/Table - DataGridView.cs
@@ -0,0 +1,15 @@
+using System;
+
+using System.Data;
+using System.Drawing;
+
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+
+
+    }
+}
