f81190f41c26ea6ef2499ff870247f2667138a2a
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-11-03 01:03:27 +0530

    Update Window Drag by Mouse.cs

--- WPF/Window Drag by Mouse.cs
+++ WPF/Window Drag by Mouse.cs
@@ -3,5 +3,8 @@ using System.Windows.Input;
 
        private void WIndow_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
         }
