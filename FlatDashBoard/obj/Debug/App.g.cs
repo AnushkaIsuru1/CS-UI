ce376bb7d7d7b5255b546bfc14db43ad5a84e9e6
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-11-04 01:10:37 +0530

    Update App.g.cs

--- FlatDashBoard/obj/Debug/App.g.cs
+++ FlatDashBoard/obj/Debug/App.g.cs
@@ -67,7 +67,12 @@ namespace FlatDashBoard {
             #line hidden
         }
         
-
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
 
     }
 }
