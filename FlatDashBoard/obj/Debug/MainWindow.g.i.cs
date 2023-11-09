799bf1a5019b8a4d55b07b09beab6dca1361fcae
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-11-10 01:16:31 +0530

    Update MainWindow.g.i.cs

--- FlatDashBoard/obj/Debug/MainWindow.g.i.cs
+++ FlatDashBoard/obj/Debug/MainWindow.g.i.cs
@@ -47,7 +47,9 @@ namespace FlatDashBoard {
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
-
+        public void InitializeComponent() {
+  
+        }
 
     }
 }
