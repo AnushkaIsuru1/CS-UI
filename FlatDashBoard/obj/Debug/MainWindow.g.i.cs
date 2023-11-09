506d936b09fa2c40d1d5f927445175a125698a26
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-11-10 01:16:38 +0530

    Update MainWindow.g.i.cs

--- FlatDashBoard/obj/Debug/MainWindow.g.i.cs
+++ FlatDashBoard/obj/Debug/MainWindow.g.i.cs
@@ -54,7 +54,11 @@ namespace FlatDashBoard {
             _contentLoaded = true;
             System.Uri resourceLocater = new System.Uri("/FlatDashBoard;component/mainwindow.xaml", System.UriKind.Relative);
             
-   
+            #line 1 "..\..\MainWindow.xaml"
+            System.Windows.Application.LoadComponent(this, resourceLocater);
+            
+            #line default
+            #line hidden
         }
 
     }
